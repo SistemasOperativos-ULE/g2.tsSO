afa9405f2decde8a0e45afeb5f45cf2d0bd05167
alopezarias <alopea08@estudiantes.unileon.es>
2019-12-17 13:05:40 +0100

    Revision del codigo de prueba y 'Core dump' resuelto

--- pruebaTsunami2(3).c
+++ pruebaTsunami2(3).c
@@ -23,6 +23,10 @@
 #define ATENDIENDO 1
 #define ATENDIDO 2
 
+//#####################################################################################################################################
+//declaracion de las estructuras
+//#####################################################################################################################################
+
 struct solicitud{
 	int id;
 	int tipo; //INVITACION = 1; QR = 0
@@ -31,15 +35,6 @@ struct solicitud{
 	int tipoDatos; //Atencion correcta, errores en los datos o antecedentes policiales
 };
 
-int idSolicitud = 1;
-
-//Inicialmente se pone al tamanio por defecto, puede que haya que cambiarlo en la parte extra de la practica (modificacion dinamica)
-int tamCola = TAMCOLADEFECTO;
-//int numSolicitudes = 0; //contador que usamos para movernos entre las solicitudes, hilos, y las struct
-bool peticionSolicitudes = true;
-
-pthread_mutex_t datosSolicitud;
-
 struct atendedor{
 	int id;
 	int tipo; //INVITACION = 1; QR = 0, PRO=3;
@@ -47,24 +42,134 @@ struct atendedor{
 	pthread_t tid;
 };
 
+
+//#####################################################################################################################################
+//declaracion de las variables globales
+//#####################################################################################################################################
+
+int idSolicitud = 1;
 int idAtendedor= 1;
 
-struct atendedor *atendedores; //Punteros para que se pueda modificar el numero de atendedores dinamicamente
-int numeroAtendedores = 3;
-//Contador de solicitudes
 struct solicitud *cola; //El tamanyo en principio es 15, pero puede variar
+struct atendedor *atendedores; //Punteros para que se pueda modificar el numero de atendedores dinamicamente
 
-//pthread_t *hilos; //TODOS LOS HILOS EN UN SOLO PUNTERO???
-//struct solicitud *solicitudes; //ES NECESARIO??? YO USARIA SOLO LA COLA DE SOLICITUDES
+//Inicialmente se pone al tamanio por defecto, puede que haya que cambiarlo en la parte extra de la practica (modificacion dinamica)
+int tamCola = TAMCOLADEFECTO;
+int numeroAtendedores = 3;
+//int numSolicitudes = 0; //contador que usamos para movernos entre las solicitudes, hilos, y las struct
+
+bool peticionSolicitudes = true;
+
+//pthread_mutex_t datosSolicitud;
+//Contador de solicitudes
+
+pthread_mutex_t crearAtendedor;
+
+//#####################################################################################################################################
+//declaracion de las funciones del programa
+//#####################################################################################################################################
+
 int posicionSiguiente(int tipoCola);
 void nuevaSolicitud(int tipo);
 int calculaAleatorio(int min, int max);
 int generadorID(int tipo);
 void *accionesSolicitud(void *arg);
+void *accionesAtendedor(void *arg);
 bool descartar(int val);
 void borrarEstructura(void *args);
 void nuevoAtendedor(int tipo);
 int buscarSiguiente(int tipo);
+
+
+//#####################################################################################################################################
+//MAIN
+//#####################################################################################################################################
+
+int main(int argc, char *argv[]){
+	int siguiente;
+	struct sigaction sLlegaSolicitud;
+	sLlegaSolicitud.sa_handler = nuevaSolicitud;//Se asigna la manejadora nuevaSolicitud a la estrutura sigaction
+
+	cola = (struct solicitud *)malloc(sizeof(struct solicitud)*(TAMCOLADEFECTO));
+	atendedores = (struct atendedor *)malloc(sizeof(struct atendedor)*(numeroAtendedores));
+	//Inicializamos los id de las solicitudes a -1 para comprobar la posicion siguiente mas adelante
+
+	if(sigaction(SIGINT, &sLlegaSolicitud, NULL) == -1){ //Enmascaramos la senal SIGUSR1 (a partir de ahora se llamara a nuevaSolicitud cuando reciba SIGUSR1)
+		perror("Error al comunicar la llegada de una nueva solicitud");
+		exit(-1);
+	}
+
+	if(sigaction(SIGQUIT, &sLlegaSolicitud, NULL) == -1){ //Enmascaramos la senal SIGUSR1 (a partir de ahora se llamara a nuevaSolicitud cuando reciba SIGUSR2)
+		perror("Error al comunicar la llegada de una nueva solicitud");
+		exit(-1);
+	}
+
+	if(sigaction(SIGTSTP, &sLlegaSolicitud, NULL) == -1){ //Enmascaramos la senal SIGUSR1 (a partir de ahora se llamara a nuevaSolicitud cuando reciba SIGUSR2)
+		perror("Error al comunicar la llegada de una nueva solicitud");
+		exit(-1);
+	}
+
+	//TODO enmascarar SIGPIPE que aumente numero de atendedores
+
+	//TODO inicializar fichero de logs
+
+	
+
+	idSolicitud = 1;
+	tamCola = TAMCOLADEFECTO;
+	peticionSolicitudes = true;
+	idAtendedor= 1;
+	numeroAtendedores = 3;
+
+	for(int i = 0; i < tamCola; i++){
+		cola[i].id = -1;
+	}
+
+	for(int i = 0; i < numeroAtendedores; i++){
+		atendedores[i].id = -1;
+	}
+
+	//CREAMOS LOS ATENDEDORES
+	nuevoAtendedor(INVITACION);
+
+	nuevoAtendedor(QR);
+
+	for(int i=3;i<=numeroAtendedores;i++){
+		nuevoAtendedor(PRO);
+	}
+	
+	//TODO crear coordinador
+
+	while(peticionSolicitudes == true){
+		pause();
+	}
+
+	printf("Una vez creados todos las solicitudes, esto es lo que tenemos en el sistema:\n\n");
+	for(int i=0; i<posicionSiguiente(SOLICITUD)-1; i++){
+		printf("-----------------------------------------\n");
+		printf("ID --> %d\n", (*(cola+i)).id);
+		printf("ATENDIDO --> %d\n", (*(cola+i)).atendido);
+		if((*(cola+i)).tipo == QR)
+			printf("TIPO --> QR\n");
+		else
+			printf("TIPO --> Invitación\n");
+	}
+	printf("-----------------------------------------\n\n");
+	/*for(int i=0; i<numSolicitudes; i++){
+		pthread_join(*(hilos+i), NULL);
+	}*/
+	free(cola);
+	printf("Todo esta cumplido, podemos cerrar\n");
+
+	//pthread_join(*(hilos+0), NULL);
+	//printf("Hilo 1 muerto\n");
+	//pthread_exit(NULL);
+	return 0;
+}
+
+//#####################################################################################################################################
+//HILOS
+//#####################################################################################################################################
 
 
 void *accionesSolicitud(void *arg){ //Funcion que ejecutan los hilos al crearse
@@ -101,7 +206,7 @@ void *accionesSolicitud(void *arg){ //Funcion que ejecutan los hilos al crearse
 	struct solicitud *s; //la estructura a la que apunta el puntero que le pasamos al hilo
 	s = (struct solicitud *) arg;
 
-	pthread_mutex_lock(&datosSolicitud);
+	//pthread_mutex_lock(&datosSolicitud);
 
 		//imprimimos los datos que tiene la estructura asociada al hilo
 	printf("Soy una solicitud, y mi id es %d\n", (*s).id);
@@ -111,7 +216,7 @@ void *accionesSolicitud(void *arg){ //Funcion que ejecutan los hilos al crearse
 		printf("Soy de tipo QR\n");
 	printf("Atendido = %d", s->atendido);
 
-	pthread_mutex_unlock(&datosSolicitud);
+	//pthread_mutex_unlock(&datosSolicitud);
 
 	//TODO Guardar en el log
 
@@ -120,7 +225,7 @@ void *accionesSolicitud(void *arg){ //Funcion que ejecutan los hilos al crearse
 	//Ahora viene la parte en la que la solicitud comprueba cada 3 segundos si ha sido aceptada o rechazada
 
 	//DUUUUUUUUUUUUUUDAAAAAAAAAAAAAAAAAAAA
-	pthread_mutex_lock(&datosSolicitud);
+	//pthread_mutex_lock(&datosSolicitud);
 	while(s->atendido == PORATENDER){
 		//Comprobamos el comportamiento de la solicitud
 		if (descartar(s->tipo)== false){
@@ -131,7 +236,7 @@ void *accionesSolicitud(void *arg){ //Funcion que ejecutan los hilos al crearse
 			pthread_exit(NULL);
 		}
 	}
-	pthread_mutex_unlock(datosSolicitud);
+	//pthread_mutex_unlock(datosSolicitud);
 
 	
 	//OJO A COMO SE HACE ESTA ESPERA
@@ -147,16 +252,20 @@ void *accionesSolicitud(void *arg){ //Funcion que ejecutan los hilos al crearse
 
 	//Cuando sale del while es que tiene uno de los 2 a true
 	//aqui el atendedor ya ha seteado la clase de solicitud que somos
-	if(s->tipoDatos==ATENCIONCORRECTA)
+	if(s->tipoDatos==ATENCIONCORRECTA){
 		printf("Mis datos estan correctos\n");
-	else if(s->tipoDatos==ERRORESDATOS)
+	}
+	else if(s->tipoDatos==ERRORESDATOS){
 		printf("Mis datos estan con errores\n");
-	else if(s->tipoDatos==ANTECEDENTES)
+	}
+	else if(s->tipoDatos==ANTECEDENTES){
 		printf("TENGO ANTECEDENTES!!\n");
+	}else{
+
+	}
 
 	//      TTTTTTTTTTTTTTOOOOOOOOOOOOOODDDDDDDDDDDDDDDDDDDDDDOOOOOOOOOOOOOOO
 	// AQUI HAY QUE ESPERAR A QUE ACABE DE SER ATENDIDO
--
 	if(s->tipoDatos == ANTECEDENTES){ //Este tipo de solicitud no puede participar en actividades sociales, asique se va pa su casa
 		//Inicializamos todos los parametros de la estructura del hilo a cero
 		borrarEstructura(&s);
@@ -179,19 +288,6 @@ void *accionesSolicitud(void *arg){ //Funcion que ejecutan los hilos al crearse
 	//TO DO//wait(coordinador.me.mata.o.coordinador.me.mata)
 	
 	pthread_exit(NULL);
-}
-
-int obtenerTipoDatos(struct solicitud *s;){
-	int tipoDatos;
-
-	pthread_mutex_lock(datosSolicitud);
-
-	tipoDatos = s->tipoDatos;
-
-	pthread_mutex_unlock(datosSolicitud);
-
-	return tipoDatos;
-
 }
 
 void *accionesAtendedor(void *arg){
@@ -254,6 +350,75 @@ void *accionesAtendedor(void *arg){
 	**/
 }
 
+//#####################################################################################################################################
+//---------------------------------------------FUNCIONES QUE YA ESTAN TESTEADAS--------------------------------------------------------
+//#####################################################################################################################################
+
+/*
+* FUNCION QUE CREA LAS SOLICITUDES SEGUN LA SEÑAL QUE RECIBIMOS
+*/
+void nuevaSolicitud(int sig){
+	
+	int siguiente= posicionSiguiente(SOLICITUD);
+	if(siguiente==-1){
+		printf("No se puede anyadir otra solicitud\n");
+	}else{
+		(*(cola+siguiente)).id = generadorID(SOLICITUD);
+		(*(cola+siguiente)).atendido = PORATENDER;
+
+		if(sig == SIGINT){ //SIGUSR1 -- invitación
+			(*(cola+siguiente)).tipo = INVITACION;
+			pthread_create(&(*(cola+siguiente)).tid, NULL, accionesSolicitud, &*(cola+siguiente));
+
+		}else if(sig == SIGQUIT){ //SIGUSR2 -- QR
+			(*(cola+siguiente)).tipo = QR;
+			pthread_create(&(*(cola+siguiente)).tid, NULL, accionesSolicitud, &*(cola+siguiente));
+
+		}else if(sig == SIGTSTP){
+			peticionSolicitudes = false; //para dejar de escuchar señales de cualquier sitio(salir del pause)
+		}
+	}
+}
+
+/*
+* FUNCION QUE CREA LOS ATENDEDORES SEGUN EL TIPO QUE SEAN
+*/
+void nuevoAtendedor(int tipo){
+
+	int siguiente;
+	siguiente= posicionSiguiente(ATENDEDOR);
+	if(siguiente == -1){
+		printf("No se puede anyiadir otro atendedor\n");
+	}else{
+		(*(atendedores+siguiente)).id = generadorID(ATENDEDOR);
+		(*(atendedores+siguiente)).tipo = tipo;
+		(*(atendedores+siguiente)).numSolicitudes = 0;
+		pthread_create(&(*(atendedores+siguiente)).tid, NULL, accionesAtendedor, &*(atendedores+siguiente));
+	}
+}
+
+
+
+//#####################################################################################################################################
+//DESARROLLO DE LAS FUNCIONES RESTANTES
+//#####################################################################################################################################
+
+
+/*int obtenerTipoDatos(void *args){
+
+	struct solicitud *s; //la estructura a la que apunta el puntero que le pasamos al hilo
+	s = (struct solicitud *) args;
+	int tipoDatos;
+
+//	pthread_mutex_lock(datosSolicitud);
+
+	tipoDatos = s->tipoDatos;
+
+//	pthread_mutex_unlock(datosSolicitud);
+
+	return tipoDatos;
+
+}*/
 
 int buscarSiguiente(int tipo){
 	int encontrado = -1;
@@ -272,7 +437,6 @@ int buscarSiguiente(int tipo){
 					}
 				}
 				break;
-
 			default:
 				while(!busquedaTerminada){
 					if(cola[i].atendido == PORATENDER && cola[i].tipo == tipo){
@@ -282,7 +446,6 @@ int buscarSiguiente(int tipo){
 						i++;
 					}
 				}
-
 				if(encontrado == -1){
 					return buscarSiguiente(PRO);
 				}
@@ -307,124 +470,9 @@ void borrarEstructura(void *args){
 
 }
 
-int main(int argc, char *argv[]){
-	int siguiente;
-	struct sigaction sLlegaSolicitud;
-	sLlegaSolicitud.sa_handler = nuevaSolicitud;//Se asigna la manejadora nuevaSolicitud a la estrutura sigaction
 
 
-	if(sigaction(SIGINT, &sLlegaSolicitud, NULL) == -1){ //Enmascaramos la senal SIGUSR1 (a partir de ahora se llamara a nuevaSolicitud cuando reciba SIGUSR1)
-		perror("Error al comunicar la llegada de una nueva solicitud");
-		exit(-1);
-	}
 
-	if(sigaction(SIGQUIT, &sLlegaSolicitud, NULL) == -1){ //Enmascaramos la senal SIGUSR1 (a partir de ahora se llamara a nuevaSolicitud cuando reciba SIGUSR2)
-		perror("Error al comunicar la llegada de una nueva solicitud");
-		exit(-1);
-	}
-
-	if(sigaction(SIGTSTP, &sLlegaSolicitud, NULL) == -1){ //Enmascaramos la senal SIGUSR1 (a partir de ahora se llamara a nuevaSolicitud cuando reciba SIGUSR2)
-		perror("Error al comunicar la llegada de una nueva solicitud");
-		exit(-1);
-	}
-
-	//TODO enmascarar SIGPIPE que aumente numero de atendedores
-
-	//TODO inicializar fichero de logs
-
-	cola = (struct solicitud *)malloc(sizeof(struct solicitud)*(TAMCOLADEFECTO));
-	atendedores = (struct atendedor *)malloc(sizeof(struct atendedor)*(numeroAtendedores));
-	//Inicializamos los id de las solicitudes a -1 para comprobar la posicion siguiente mas adelante
-
-	idSolicitud = 1;
-	tamCola = TAMCOLADEFECTO;
-	peticionSolicitudes = true;
-	idAtendedor= 1;
-	numeroAtendedores = 3;
-
-	for(int i = 0; i < TAMCOLADEFECTO; i++){
-		cola[i].id = -1;
-	}
-
-	//CREAMOS LOS ATENDEDORES
-	nuevoAtendedor(INVITACION);
-
-	nuevoAtendedor(QR);
-
-	for(int i=3;i<=numeroAtendedores;i++){
-		nuevoAtendedor(PRO);
-	}
-	
-	//TODO crear coordinador
-
-	while(peticionSolicitudes == true){
-		pause();
-	}
-
-	printf("Una vez creados todos las solicitudes, esto es lo que tenemos en el sistema:\n\n");
-	for(int i=0; i<posicionSiguiente(SOLICITUD)-1; i++){
-		printf("-----------------------------------------\n");
-		printf("ID --> %d\n", (*(cola+i)).id);
-		printf("ATENDIDO --> %d\n", (*(cola+i)).atendido);
-		if((*(cola+i)).tipo == QR)
-			printf("TIPO --> QR\n");
-		else
-			printf("TIPO --> Invitación\n");
-	}
-	printf("-----------------------------------------\n\n");
-	/*for(int i=0; i<numSolicitudes; i++){
-		pthread_join(*(hilos+i), NULL);
-	}*/
-	free(cola);
-	printf("Todo esta cumplido, podemos cerrar\n");
-
-	//pthread_join(*(hilos+0), NULL);
-	//printf("Hilo 1 muerto\n");
-	//pthread_exit(NULL);
-	return 0;
-}
-
-
-/*
-* FUNCION QUE CREA LOS ATENDEDORES SEGUN EL TIPO QUE SEAN
-*/
-void nuevoAtendedor(int tipo){
-
-	int siguiente;
-
-	siguiente= posicionSiguiente(ATENDEDOR);
-	(*(atendedores+siguiente)).id = generadorID(ATENDEDOR);
-	(*(atendedores+siguiente)).tipo = tipo;
-	(*(atendedores+siguiente)).numSolicitudes = 0;
-	pthread_create(&(*(atendedores+siguiente)).tid, NULL, accionesAtendedor, &*(atendedores+siguiente));
-
-}
-
-	
-void nuevaSolicitud(int sig){
-	int siguiente= posicionSiguiente(SOLICITUD);
-	//LO PRIMERO QUE HAY QUE COMPROBAR ES SI HAY HUECO PARA ENTRAR EN LA COLA
-	if(posicionSiguiente(SOLICITUD)==tamCola){
-		//LOG
-	}
-	else{
-
-		(*(cola+siguiente)).id = generadorID(SOLICITUD);
-		(*(cola+siguiente)).atendido = PORATENDER;
-
-		if(sig == SIGINT){ //SIGUSR1 -- invitación
-			(*(cola+siguiente)).tipo = INVITACION;
-			pthread_create(&(*(cola+siguiente)).tid, NULL, accionesSolicitud, &*(cola+siguiente));
-
-		}else if(sig == SIGQUIT){ //SIGUSR2 -- QR
-			(*(cola+siguiente)).tipo = QR;
-			pthread_create(&(*(cola+siguiente)).tid, NULL, accionesSolicitud, &*(cola+siguiente));
-
-		}else if(sig == SIGTSTP){
-			peticionSolicitudes = false;
-		}
-	}
-}
 
 int posicionSiguiente(int tipoCola){
 
@@ -432,7 +480,7 @@ int posicionSiguiente(int tipoCola){
 	bool encontrado;
 
 	//pthread_mutex_lock();
-	i=-1;
+	i = 0;
 	encontrado = false;
 	switch(tipoCola){
 		case 0://Cola solicitudes
@@ -443,9 +491,12 @@ int posicionSiguiente(int tipoCola){
 					i++;
 				}
 			}
+
+			if(i == tamCola){
+				i = -1;
+			}
 			break;
 		case 1://Cola atendedores
-			i=0;
 			while(i < numeroAtendedores && !encontrado){
 				if(atendedores[i].id == -1){
 					encontrado = true;
@@ -453,9 +504,14 @@ int posicionSiguiente(int tipoCola){
 					i++;
 				}
 			}
+
+			if(i == numeroAtendedores){
+				i = -1;
+			}
 			break;
 	}
 	//pthread_mutex_unlock();
+	printf("El tipo de cola %d da posicion %d\n", tipoCola, i);
 	return i;
 	
 }
